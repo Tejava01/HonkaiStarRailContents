77f0f161ca3406ca3b795a8383329890528e8b8a
Tejava01 <hewk0528@naver.com>
2025-05-24 00:27:41 +0900

    레이저회피 씬세팅

---
+++ HonkaiStarRailContents/Assets/02.Scripts/LaserEvasionSimulator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserEvasionSimulator : MonoBehaviour
+{
+    [SerializeField, Min(1)] private int totalRound = 6;
+    [SerializeField] private int goal1 = 4;
+    [SerializeField] private int goal2 = 6;
+
+    //----------------------------------------------------
+
+    private void OnValidate()
+    {
+        SettingGoal();
+    }
+
+    //----------------------------------------------------
+
+    private void SettingGoal()
+    {
+        goal1 = Mathf.Clamp(goal1, 1, totalRound);
+        goal2 = Mathf.Clamp(goal2, 1, totalRound);
+    }
+}
---
+++ HonkaiStarRailContents/Assets/02.Scripts/Player/PlayerMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private int speed = 10;
+
+    void Update()
+    {
+        Move();
+    }
+
+    //----------------------------------------------
+
+    private void Move()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            gameObject.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            gameObject.transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+    }
+}
